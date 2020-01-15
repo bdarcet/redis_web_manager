58dcf4a0e8a2d61e55f22fee568b156a5458ac35
DumasOlivier <dumas.olivier@outlook.fr>
2020-01-15 21:25:58 +0100

    dashboard controller

---
+++ app/controllers/redis_web_manager/dashboard_controller.rb
@@ -0,0 +1,7 @@
+require_dependency "redis_web_manager/application_controller"
+
+module RedisWebManager
+  class DashboardController < ApplicationController
+    def index; end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 RedisWebManager::Engine.routes.draw do
+  # Root
+  root 'dashboard#index'
 end
