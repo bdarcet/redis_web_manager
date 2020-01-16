fc5730b04b1ca5f997b127630888008778c9c3b0
Benjamin Darcet <b.darcet@gmail.com>
2020-01-16 16:19:02 +0100

    refactor dashboard controller

--- app/controllers/redis_web_manager/dashboard_controller.rb
+++ app/controllers/redis_web_manager/dashboard_controller.rb
@@ -2,8 +2,15 @@
 
 module RedisWebManager
   class DashboardController < ApplicationController
+    INFORMATIONS = { version: 'redis_version',
+                     role: 'role',
+                     used_memory: 'used_memory_human',
+                     total_system_memory: 'total_system_memory_human' }.freeze
+
     def index
-      #@version = RedisWebManager::Base.new.info['redis_version']
+      INFORMATIONS.each do |key, value|
+        instance_variable_set("@#{key}", RedisWebManager::Base.new.info[value])
+      end
       @status = RedisWebManager::Base.new.status
     end
   end
