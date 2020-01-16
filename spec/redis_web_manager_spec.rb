4f25d49e1b46540c01185b1b6edc686d664e792f
BorisBresciani <boris.bresciani@epitech.eu>
2020-01-16 16:56:13 +0100

    Remove useless tests

--- spec/redis_web_manager_spec.rb
+++ spec/redis_web_manager_spec.rb
@@ -8,24 +8,11 @@ RSpec.describe RedisWebManager do
       expect(RedisWebManager.configuration.redis).to be_a_kind_of(Redis)
     end
 
-    it 'returns a Redis class (Reset)' do
-      RedisWebManager.reset
-      expect(RedisWebManager.configuration.redis).to be_a_kind_of(Redis)
-    end
-
     it 'returns a valid db' do
       RedisWebManager.configure do |config|
         config.redis = Redis.new(db: 1)
       end
       expect(RedisWebManager.configuration.redis.connection[:db]).to eql(1)
-    end
-
-    it 'returns a raise error' do
-      expect do
-        RedisWebManager.configure do |config|
-          config.redis = 'Test'
-        end
-      end.to raise_error(ArgumentError)
     end
   end
 end
