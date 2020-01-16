b3a79eb4c8465d4d0728bc76220b6ebaa929e04b
DumasOlivier <dumas.olivier@outlook.fr>
2020-01-16 14:14:41 +0100

    dummy test

--- spec/controllers/redis_web_manager/dashboard_controller_spec.rb
+++ spec/controllers/redis_web_manager/dashboard_controller_spec.rb
@@ -4,6 +4,10 @@ require 'rails_helper'
 
 module RedisWebManager
   RSpec.describe DashboardController, type: :controller do
-
+    describe 'index method' do
+      it '@status respond with a boolean type' do
+        #FIXME: Test vars
+      end
+    end
   end
 end
