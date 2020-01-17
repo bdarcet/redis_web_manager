ba75dfad845a364b06988a62ac591a4be5c8ddd5
BorisBresciani <boris.bresciani@epitech.eu>
2020-01-17 11:54:13 +0100

    Refactor status / html

--- app/helpers/redis_web_manager/application_helper.rb
+++ app/helpers/redis_web_manager/application_helper.rb
@@ -2,5 +2,12 @@
 
 module RedisWebManager
   module ApplicationHelper
+    def status(value)
+      if value
+        content_tag(:span, 'ON', class: 'badge badge-pill badge-success')
+      else
+        content_tag(:span, 'OFF', class: 'badge badge-pill badge-danger')
+      end
+    end
   end
 end
