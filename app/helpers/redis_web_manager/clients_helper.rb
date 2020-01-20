816a3bf1e508b1d4c33113d03f35c1261c9a98fe
BorisBresciani <boris.bresciani@epitech.eu>
2020-01-20 17:00:52 +0100

    Fixs

--- app/helpers/redis_web_manager/clients_helper.rb
+++ app/helpers/redis_web_manager/clients_helper.rb
@@ -9,26 +9,26 @@ module RedisWebManager
     def flags(value)
       {
         A: 'Connection to be closed ASAP',
-        b: 'Client is waiting in a blocking operation',
+        b: 'The client is waiting in a blocking operation',
         c: 'Connection to be closed after writing entire reply',
-        d: 'Watched keys has been modified - EXEC will fail',
-        i: 'Client is waiting for a VM I/O (deprecated)',
-        M: 'Client is a master',
+        d: 'A watched keys has been modified - EXEC will fail',
+        i: 'The client is waiting for a VM I/O (deprecated)',
+        M: 'The client is a master',
         N: 'No specific flag set',
-        O: 'Client is a client in MONITOR mode',
-        P: 'Client is a Pub/Sub subscriber',
-        r: 'Client is in readonly mode against a cluster node',
-        S: 'Client is a replica node connection to this instance',
-        u: 'Client is unblocked',
-        U: 'Client is connected via a Unix domain socket',
-        X: 'Client is in a MULTI/EXEC context'
+        O: 'The client is a client in MONITOR mode',
+        P: 'The client is a Pub/Sub subscriber',
+        r: 'The client is in readonly mode against a cluster node',
+        S: 'The client is a replica node connection to this instance',
+        u: 'The client is unblocked',
+        U: 'The client is connected via a Unix domain socket',
+        X: 'The client is in a MULTI/EXEC context'
       }[value.to_sym]
     end
 
     def events(value)
       {
-        r: 'Readable',
-        w: 'Writable'
+        r: 'The client socket is readable',
+        w: 'The client socket is writable'
       }[value.to_sym]
     end
   end
