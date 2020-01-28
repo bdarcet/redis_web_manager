13d9256400a9ae2ab6e0faa3e18c49b12d58e4b9
BorisBresciani <boris.bresciani@epitech.eu>
2020-01-28 14:09:33 +0100

    Realod URL

--- app/assets/javascripts/redis_web_manager/keys.js
+++ app/assets/javascripts/redis_web_manager/keys.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
+    const url = document.location.toString();
+    if (url.match('#')) {
+        $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+    }
     $('.nav-tabs a').on('click', function () {
+        window.location.hash = this.target.hash;
         $(this).tab('show');
     });
 });
