d36ee6df54c3e6afd914503f376aa9badfd5eabe
BorisBresciani <boris.bresciani@epitech.eu>
2020-01-31 18:07:52 +0100

    Remove allocation

--- app/helpers/redis_web_manager/dashboard_helper.rb
+++ app/helpers/redis_web_manager/dashboard_helper.rb
@@ -2,13 +2,19 @@
 
 module RedisWebManager
   module DashboardHelper
-    def graph_canvas(data)
-      if data.empty?
+    def graph_canvas(data, id)
+      if data.nil? || data.empty?
         # FIXME: Put the link of the doc where we explain how to call the perform method from data.rb.
-        content_tag(:div, 'You don\'t have any RedisWebManager keys yet into your redis database.', class:'m-5 text-center')
+        content_tag(:div,
+                    'You don\'t have any RedisWebManager keys yet into your redis database.',
+                    class: 'm-5 text-center')
       else
-        id = data[1].keys[1].to_s
-        content_tag(:canvas, '', id: id, width: 800, height: 400, data: { canvas: data.to_json })
+        content_tag(:canvas,
+                    nil,
+                    id: id,
+                    width: 800,
+                    height: 400,
+                    data: { canvas: data.to_json })
       end
     end
   end
