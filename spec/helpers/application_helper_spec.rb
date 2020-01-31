52b7e20e63cc0746aad1b9b0575fa61854f0e7e7
Boris Bresciani <boris2bresciani@gmail.com>
2020-02-01 00:52:01 +0100

    Add test helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,6 +4,16 @@ require 'rails_helper'
 
 RSpec.describe RedisWebManager::ApplicationHelper, type: :helper do
   describe 'helper' do
+    it 'returns status tag (true)' do
+      expect(helper.status(true)).to match(/ON/)
+    end
 
+    it 'returns status tag (true)' do
+      expect(helper.status(false)).to match(/OFF/)
+    end
+
+    it 'returns url tag' do
+      expect(helper.url('test.com')).to match(/kbd/)
+    end
   end
 end
