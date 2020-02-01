d5a15722c0151066ed96f9c05ee5aefeed6a6f4e
Boris Bresciani <boris2bresciani@gmail.com>
2020-02-01 22:24:02 +0100

    Update application_helper_spec.rb

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -15,5 +15,13 @@ RSpec.describe RedisWebManager::ApplicationHelper, type: :helper do
     it 'returns url tag' do
       expect(helper.url('test.com')).to match(/kbd/)
     end
+
+    it 'returns a no expiration' do
+      expect(helper.expire(-1)).to eql('No expiration date')
+    end
+
+    it 'returns a expiration' do
+      expect(helper.expire(86_400)).to eql('1 day')
+    end
   end
 end
